c373b5044195f59d1002417831e28f291b72d126
Alexander Pollack <pollack0@compute5.physics.umd.edu>
2019-04-21 12:52:23 -0400

    Confirmed cloning of functions works properly (step 3)

--- pollack/sample/lib/Hello/Hello.cpp
+++ pollack/sample/lib/Hello/Hello.cpp
@@ -63,17 +63,36 @@ struct Hello :  public FunctionPass
 			for(BasicBlock::iterator i = BB->begin(), ie = BB->end(); i != ie; ++i){
 				Instruction* IN = i;
 				if(isa<CallInst>(*IN)){
-					errs() << *IN << "\n";
+					//errs() << *IN << "\n";
 					CallInst *CI = cast<CallInst>(IN);
 					Function* callingFunc = CI->getCalledFunction();
-					if(callingFunc->getName().front() == 'p'){
+					if(callingFunc->getName().front() == 'p' && callingFunc->getName() != "printf"){
 						errs() << "Cloning " << callingFunc->getName() << "\n";
 						llvm::ValueToValueMapTy VMap;
-						ClonedCodeInfo *CodeInfo;
-						// TODO: Fix
+						llvm::ClonedCodeInfo *CodeInfo = (ClonedCodeInfo *)malloc(sizeof(ClonedCodeInfo));
+						// TODO: Determine if correct assignment to CodeInfo
 						CodeInfo->ContainsCalls = false;
 						CodeInfo->ContainsDynamicAllocas = false;
-						Function *clonedFunc = llvm::CloneFunction(callingFunc, VMap, false, CodeInfo);
+						Function *clonedFunc = llvm::CloneFunction(callingFunc, VMap, CodeInfo);
+
+						// For testing purposes - Make sure cloned functions are identical
+						/*
+						for(Function::iterator o = callingFunc->begin(), oe = callingFunc->end(); o != oe; ++o){
+							BasicBlock* OB = o;
+							for(BasicBlock::iterator oI = OB->begin(), oIE = OB->end(); oI != oIE; ++oI){
+								Instruction *origInstr = oI;
+								errs() << *origInstr << "\n";
+							} 
+						}
+						errs() << "Printing cloned function:\n";
+						for(Function::iterator c = clonedFunc->begin(), ce = clonedFunc->end(); c != ce; ++c){
+							BasicBlock *CB = c;
+							for(BasicBlock::iterator cI = CB->begin(), cIE = CB->end(); cI != cIE; ++cI){
+								Instruction *clonInstr = cI;
+								errs() << *clonInstr << "\n";
+							}
+						}
+						*/
 					}
 				}
 			}
@@ -84,7 +103,7 @@ struct Hello :  public FunctionPass
 }
 
 char Hello::ID = 0;
-static RegisterPass<Hello> X("hello", "Hello World Pass", false, false);
+static RegisterPass<Hello> X("hello", "Clone Test Pass", false, false);
 
 
 
